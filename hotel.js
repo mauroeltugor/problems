97224fe1dbd0916b4ce4091fd2236f316a44e87c
mauro <andrekxus299@gmail.com>
2023-03-20 17:25:24 -0500

    correct errors and add coments

--- hotel.js
+++ hotel.js
@@ -1,22 +1,23 @@
-const occupiedRooms = [];
+//create all the variables
+const unaveilableRooms = [];
 const maxRooms = 20;
-let requiredRooms = 21;
+const maxHour = 12;
+let requiredRooms = 8;
 let checkinHour = 11;
 
-if (requiredRooms <= maxRooms && checkinHour <= 12) {
-    let availableRoom = maxRooms - requiredRooms;
+//in this if we see if we have available rooms and if is the hour 
+if (requiredRooms <= maxRooms && checkinHour <= maxHour) {
+    let availableRoom = maxRooms - requiredRooms;//subtract the required to the max rooms  
     if (availableRoom >= requiredRooms) {
         for (let i = 0; i < requiredRooms; i += 1) {
-            occupiedRooms.push(i);
+            unaveilableRooms.push(i);
         }
-        console.log(`Rooms selected with success, there are ${occupiedRooms} available rooms left`);
-        console.log(availableRoom);
-    } else {
+        console.log(`Rooms selected with success, there are ${unaveilableRooms.length} rooms ocuppied and ${availableRoom} available`);//print the number of rooms that are available 
+    } else {// is when we have not available rooms 
         console.log('No rooms available');
     }
-} else if (requiredRooms > 20 || checkinHour > 12) {
-    console.log('Sorry the reservation must be made before 12');
+} else if (checkinHour > 12) {//when the reservation is after 12 
+    console.log('you can not reserve rooms after 12');
 } else {
     console.log('No rooms available');
 }
-
